3a71123e882a108eae075a6a5ad75f3acbdd01d9
Роман <110688570+MoskalchenkoRoman@users.noreply.github.com>
2023-08-13 13:33:37 +0300

    dz11

---
+++ src/test/java/dz3/CarTest.java
@@ -0,0 +1,14 @@
+package dz3;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarTest {
+
+    @org.junit.jupiter.api.Test
+    void getColor() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setColor() {
+    }
+}
